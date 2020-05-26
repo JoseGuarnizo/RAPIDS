6ff6f758d1125e82f87ee4a75821c9e23edb90b1
JoseGuarnizo <joseguarnizo1996@gmail.com>
2020-05-26 09:35:22 -0500

    Actualizando

--- ParsePandas/Untitled.py
+++ ParsePandas/Untitled.py
@@ -11,13 +11,14 @@ import json
 # In[2]:
 
 
-data = pd.read_csv('logv.csv',names='uno')
-
+data = pd.read_csv('C:/Users/joseg/Desktop/RAPIDS/ParsePandas/logv.csv',names='uno')
+#print("Data Leida, aun no en un datframe\n", data)
 
 # In[3]:
 
 
 data2 = list(data['u'])
+#print("Data convertido en lista\n", data2)
 
 
 # In[7]:
@@ -32,10 +33,26 @@ for i in range(0,len(data2)):
 # In[10]:
 
 
-print(pd.DataFrame(da))
+print("Data colocado en cada columna\n", pd.DataFrame(da))
 
 
 # In[ ]:
+new_data = pd.DataFrame(da)
 
+new_data.columns
+new_data.head(10)
 
+#Creacion de datframe Filtrados
+new_data = new_data[['username', 'event_type', 'name', 'accept_language', 'time', 'agent',
+                     'host', 'session', 'referer', 'context', 'ip', 'event', 'event_source']]
+new_data.head()
 
+#verficar detalles de columnas con más de un dato
+print("Estos pertenecen columna context\n",new_data['context'])
+print("Estos pertenecen columna event\n",new_data['event'])
+
+contextData = new_data['context'].values.tolist()
+contextData
+new_contextData = pd.DataFrame(contextData, columns= ['course_id', 'course_user_tags',
+                                                      'org_id', 'path', 'user_id'])
+new_contextData.head()
